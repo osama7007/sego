f16fcf052bbbf72ab6cf906ac99c6d49123b0730
osama7007 <osamasaadelsaeed@gmail.com>
2024-03-20 10:29:58 +0200

    change auth routes

--- src/components/layput/header/Navbar.tsx
+++ src/components/layput/header/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ProfileIcon from "../../../icons/ProfileIcon";
 import Logout from "./Logout";
 const navLinks = [
@@ -51,6 +51,8 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+    const token = localStorage.getItem('sego_token')
+    const navigate = useNavigate()
     return (
         <nav className="container flex items-center justify-around pt-8 capitalize">
             <Link to='/home' >
@@ -58,13 +60,13 @@ const Navbar = () => {
             </Link>
             <ul className="flex items-center gap-x-8">
                 {navLinks.map(link => (
-                    <li key={link.route} className="relative z-20 hoverEffect group py-4">
+                    <li key={link.page} className="relative z-20 hoverEffect group py-4" hidden={!token}>
                         <Link to={link.route}>{link.page}</Link>
                         {
                             link.nestedRoutes ?
                                 <ul className="absolute invisible duration-300 translate-y-32 opacity-0 top-12 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 text-white bg-primary p-4 rounded w-[250px]">
                                     {link.nestedRoutes.map(nestedLink =>
-                                        <li className="hover:text-black my-2 duration-300"><Link to={nestedLink.route}>{nestedLink.page}</Link></li>
+                                        <li key={nestedLink.page} className="hover:text-black my-2 duration-300"><Link to={nestedLink.route}>{nestedLink.page}</Link></li>
                                     )}
                                 </ul>
                                 : null
@@ -73,11 +75,17 @@ const Navbar = () => {
                 ))
                 }
             </ul>
-            <Link to='/profile' className="flex items-center gap-x-2 hoverEffect">
-                <ProfileIcon />
-                <span>profile</span>
-            </Link>
-            <Logout />
+            {
+                token ?
+                    <div>
+                        <Link to='/profile' className="flex items-center gap-x-2 hoverEffect">
+                            <ProfileIcon />
+                            <span>profile</span>
+                        </Link>
+                        <Logout />
+                    </div>
+                    : <button onClick={() => navigate('/login')}>Login</button>
+            }
         </nav>
     )
 }
